ed07612bfcb837d151f769a6ae1f366a95f57a56
Shay Brown <fire328@ymail.com>
2020-08-29 07:47:16 -0400

    sort out styling for tasks and contact pages

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './index.css';
 import NavBar from './components/navbar';
 import About from './components/about';
 import Error from './components/error';
 import Contact from './components/contact'
+import Tasks from './components/tasks'
 
 const App = () => {
   return (
@@ -13,7 +15,7 @@ const App = () => {
         <NavBar />
         <Switch>
           <Route path="/about" component={About} />
-          <Route path="/tasks" component={About} />
+          <Route path="/tasks" component={Tasks} />
           <Route path="/contact" component={Contact} />
           <Route component={Error} />
         </Switch>
