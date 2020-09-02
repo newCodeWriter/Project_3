11e8ef3a5dac23d2cf060b9f0fc786d713a8cd88
Shay Brown <fire328@ymail.com>
2020-09-02 18:11:30 -0400

    add state to all form fields

--- my-project/src/components/contact.jsx
+++ my-project/src/components/contact.jsx
@@ -1,31 +1,38 @@
 import React from "react";
-import { Prompt } from "react-router-dom";
 
 class Contact extends React.Component {
     constructor() {
         super();
-        this.state = { value: "" };
+        this.state = {
+            fname: '',
+            lname: '',
+            email: '',
+            comments: ''
+        };
     }
-    handleChange(event) {
-        this.setState({ value: event.target.value });
+    handleChange = (event) => {
+        let name = event.target.name;
+        let val = event.target.value; 
+        this.setState({[name]: val});
     }
-    handleSubmit() {
-        this.setState({ value: "" });
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`First Name: ${this.state.fname}\nLast Name: ${this.state.lname}\nE-mail: ${this.state.email}\nComments: ${this.state.comments}`)
+        document.getElementById('my-form').reset();
     }
     render() {
         return (
             <div>
-                <form action="#">
-                    <label for="fname">First Name</label>
-                    <input type="text" name="fname" id="fname" placeholder="First Name" required/><br/>
-                    <label for="lname">Last Name</label>
-                    <input type="text" name="lname" id="lname" placeholder="Last Name" required/><br/>
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="E-mail" required/><br/>
-                    <label for="comments">Comments</label>
-                    <textarea value={this.state.value} name="comments" id="comments" placeholder="Please enter any comments here" rows="8" cols="30" onChange={this.handleChange.bind(this)}></textarea>
-                    <input type="submit" value="Submit" onClick={this.handleSubmit.bind(this)} />
-                    <Prompt when={this.state.value !== ""} message="are you sure you want to leave without sending a message?"/>
+                <form onSubmit={this.handleSubmit} id="my-form">
+                    <label htmlFor="fname">First Name</label>
+                    <input type="text" name="fname" id="fname" placeholder="First Name" onChange={this.handleChange} required/><br/>
+                    <label htmlFor="lname">Last Name</label>
+                    <input type="text" name="lname" id="lname" placeholder="Last Name" onChange={this.handleChange} required/><br/>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="E-mail" onChange={this.handleChange} required/><br/>
+                    <label htmlFor="comments">Comments</label>
+                    <textarea name="comments" id="comments" placeholder="Please enter any comments here" rows="8" cols="30" onChange={this.handleChange}></textarea>
+                    <input type="submit" value="Submit" />
                 </form>
             </div>
         );
